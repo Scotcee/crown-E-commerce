b8a08651f231eb183e4656f47c18ce54c012f5f2
Scotcee <itzscotcee@gmail.com>
2024-07-22 22:21:01 +0100

    menu items component and linking url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import HatsPage from './pages/hats/hats.component';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/hats" element={<HatsPage />} />
+    </Routes>
   );
 }
 
--- src/components/menu-item.component.jsx
+++ src/components/menu-item.component.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./menu-item/menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    ></div>
-    <div className="content">
-      <div className="title">{title}</div>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
-);
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  return (
+    <Link to={linkUrl} className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="content">
+        <div className="title">{title}</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </Link>
+  );
+};
 
 export default MenuItem;
