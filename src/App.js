8a58ecad1eef0bb3a039ddb4772803d10b914c80
Scotcee <itzscotcee@gmail.com>
2024-09-24 00:50:35 +0100

    sign-up page and creating button and flexible forms

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignupPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<SignInAndSignupPage />} />
       </Routes></>
 
   );
---
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sign-in-and-sign-up-page.styles.scss";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignupPage = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignupPage;
